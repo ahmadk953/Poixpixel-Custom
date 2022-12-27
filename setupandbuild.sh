3c46b1b826e1bf71a8ce32e255ade1091d4ec63e
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2022-12-26 21:30:44 -0500

    Delete setupandbuild.sh

--- setupandbuild.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install openjdk-17-jre-headless -y
-sudo apt install openjdk-17-jre-headless -y
-wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-tar -xvf apache-maven-3.6.3-bin.tar.gz
-mv apache-maven-3.6.3 /opt/
-mvn clean package
